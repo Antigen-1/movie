a24728fddbe9a9929c68aa74e2bc33068650fb83
Antigen-1 <zhanghao@antigen.top>
2023-03-04 12:04:46 +0800

    reverse

--- lib.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket/base
-(require simple-python-api racket/promise)
-(provide moviepy)
-
-(define moviepy (delay (let ((m (import "moviepy")))
-                         (at-exit (lambda () (decrement-reference m)))
-                         m)))

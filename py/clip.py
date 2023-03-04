aece8d4f59a91973e4b9339af76c07e7fab485b3
Antigen-1 <zhanghao@antigen.top>
2023-03-04 21:13:12 +0800

    remove

--- py/clip.py
+++ py/clip.py
@@ -12,9 +12,5 @@ def cut_clip(c, s, e):
 def subclip(c, s, e):
     return c.subclip(s, e)
 
-def map_clips(c, f, p=False, o=None, d=True):
-    func=(CFUNCTYPE(py_object, py_object, c_double))(f) if p else f 
-    return c.transform(func, o, d)
-
 def set_fps(c, f):
     return c.with_fps(f, True)
